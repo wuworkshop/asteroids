cc5929c1ed847d610c2df958d25e7f543fc14491
wuworkshop <wuworkshop@users.noreply.github.com>
2026-03-10 01:02:38 -0400

    Print screen width and screen height.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     # Initialize a new GUI window or screen for display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
